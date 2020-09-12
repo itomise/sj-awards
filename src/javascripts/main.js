99a9ec7e7a2fb880409f058607a3c71a12d7bbcc
ryota.ito <itomise.mizumi@icloud.com>
2020-09-12 17:05:22 +0900

    clean : module 整理

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -13,11 +13,41 @@ if (module.hot) {
 
 import * as THREE from 'three'
 window.THREE = THREE
-import { onxrloaded } from './modules/sample'
+import { PipelineModule } from './modules/pipelineModule'
 
-const load = () => {
-  XRExtras.Loading.showLoading({ onxrloaded })
+class XRMain {
+  constructor() {
+    this.pipelineModule = new PipelineModule()
+
+    if (window.XRExtras) {
+      XRExtras.Loading.showLoading({ onxrloaded: this.onxrloaded.bind(this) })
+    } else {
+      window.addEventListener('xrextrasloaded', this.onxrloaded.bind(this))
+    }
+  }
+
+  onxrloaded() {
+    // アプリがイメージターゲットでしかワールドを形成しない場合、
+    // disableWorldTracking を true にすることでパフォーマンスを向上できる
+    XR8.xrController().configure({ disableWorldTracking: true })
+    XR8.addCameraPipelineModules([
+      // Add camera pipeline modules.
+      // Existing pipeline modules.
+      XR8.GlTextureRenderer.pipelineModule(), // Draws the camera feed.
+      XR8.Threejs.pipelineModule(), // Creates a ThreeJS AR Scene.
+      XR8.XrController.pipelineModule(), // Enables SLAM tracking.
+      XRExtras.AlmostThere.pipelineModule(), // Detects unsupported browsers and gives hints.
+      XRExtras.FullWindowCanvas.pipelineModule(), // Modifies the canvas to fill the window.
+      XRExtras.Loading.pipelineModule(), // Manages the loading screen on startup.
+      XRExtras.RuntimeError.pipelineModule(), // Shows an error image on runtime error.
+      // Custom pipeline modules.
+      this.pipelineModule.getModule(), // Draws a frame around detected image targets.
+    ])
+
+    XR8.run({ canvas: document.getElementById('canvas') })
+  }
 }
-window.onload = () => {
-  window.XRExtras ? load() : window.addEventListener('xrextrasloaded', load)
-}
+
+window.addEventListener('load', () => {
+  new XRMain()
+})
---
+++ src/javascripts/modules/pipelineModule.js
@@ -0,0 +1,96 @@
+import * as THREE from 'three'
+
+class PipelineModule {
+  constructor() {
+    this.videoFile = `${process.env.PUBLIC_URL}/video/jellyfish-video.mp4`
+
+    this.video
+    this.videoObj
+
+    this.module = {
+      name: 'threejs-flyer',
+      onStart: this.onStart.bind(this),
+      listeners: [
+        { event: 'reality.imagefound', process: this.showTarget.bind(this) },
+        { event: 'reality.imageupdated', process: this.showTarget.bind(this) },
+        { event: 'reality.imagelost', process: this.hideTarget.bind(this) },
+      ],
+    }
+  }
+
+  getModule() {
+    return this.module
+  }
+
+  // Sceneを初期化。 カメラはXR three.js
+  initXrScene({ scene, camera }) {
+    console.log('initXrScene')
+
+    this.video = document.createElement('video')
+    this.video.src = this.videoFile
+    this.video.setAttribute('preload', 'auto')
+    this.video.setAttribute('loop', '')
+    this.video.setAttribute('muted', '')
+    this.video.setAttribute('playsinline', '')
+    this.video.setAttribute('webkit-playsinline', '')
+
+    const texture = new THREE.VideoTexture(this.video)
+    texture.minFilter = THREE.LinearFilter
+    texture.magFilter = THREE.LinearFilter
+    texture.format = THREE.RGBFormat
+    texture.crossOrigin = 'anonymous'
+
+    this.videoObj = new THREE.Mesh(
+      new THREE.PlaneGeometry(0.75, 1),
+      new THREE.MeshBasicMaterial({ map: texture })
+    )
+    // ターゲットが検出されるまで非表示に
+    this.videoObj.visible = false
+    scene.add(this.videoObj)
+    this.video.load()
+
+    // アンビエントライトを追加
+    scene.add(new THREE.AmbientLight(0x404040, 5))
+
+    // 最初のカメラ位置を設定。y=0より大きくなければならない
+    camera.position.set(0, 3, 0)
+  }
+
+  // スタート時にカメラ位置を設定
+  onStart({ canvas }) {
+    // XRからThree.jsの scene を取得
+    const { scene, camera } = XR8.Threejs.xrScene()
+
+    // sceneにコンテンツを追加、カメラの設定
+    this.initXrScene({ scene, camera })
+
+    // XRコントローラの位置とカメラのパラメータを同期
+    XR8.XrController.updateCameraProjectionMatrix({
+      origin: camera.position,
+      facing: camera.quaternion,
+    })
+  }
+
+  // コンテンツを画像ターゲットの上に配置
+  showTarget({ detail }) {
+    // detail.name で 8th wallで登録した画像の名前をとれる
+
+    if (detail.name === 'video-target') {
+      this.videoObj.position.copy(detail.position)
+      this.videoObj.quaternion.copy(detail.rotation)
+      this.videoObj.scale.set(detail.scale, detail.scale, detail.scale)
+      this.videoObj.visible = true
+      this.video.play()
+    }
+  }
+
+  // マーカーが検出されなくなったら画像フレームを非表示に
+  hideTarget({ detail }) {
+    if (detail.name === 'video-target') {
+      this.video.pause()
+      this.videoObj.visible = false
+    }
+  }
+}
+
+export { PipelineModule }
